cef0508928f20a73677e44db48ddcebea492e698
Jivesh21 <jiveshsharma2220@gmail.com>
2026-07-20 17:09:15 +0530

    New File

--- 10-FormHandling/src/App.jsx
+++ 10-FormHandling/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const App = () => {
+const SubmitHandler= (Submit)=>{
+  Submit.preventDefault()
+  console.log("form submitted")
+}
   return (
     <div>
-      
+      <input type='text' placeholder='Enter you Name'></input>
+      <form onSubmit={(Submit)=>{
+        SubmitHandler(Submit)
+      }}>
+      <button>
+        Submit
+      </button>
+      </form>
     </div>
   )
 }
